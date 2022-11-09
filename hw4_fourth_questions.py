9672814dace63cca5ae03f7de4ec87d233d57873
4Freye <4freye@gmail.com>
2022-11-09 17:07:24 +0100

    added my work

---
+++ hw4_fourth_questions.py
@@ -0,0 +1,30 @@
+###############
+# Use the data in covid.csv for this exercise
+#
+# 10) In a separate file, write a piece of code that
+# loads the covid.csv file and prints the list of countries
+#  and the total average of death/confirmed among those countries
+# for those countries that have more than 500, 1000 and 5000
+# active cases respectively.
+# Follow DRY principles in order to complete this exercise.
+#
+#
+# #
+import sys
+
+try: 
+    import pandas as pd
+except:
+    print('Pandas not installed. Please install pandas first by running: pip install pandas')
+    sys.exit()
+
+path = ''
+
+covid = pd.read_csv(path + 'covid.csv')
+
+covid['Deaths/Confirmed'] = covid.Deaths/covid.Confirmed
+
+for n in [500,1000,5000]:
+    print('''countries with > ''' + str(n) + ''' active cases:
+''', covid[covid.Active > n][['Country','Deaths/Confirmed']],'''
+          ''')
